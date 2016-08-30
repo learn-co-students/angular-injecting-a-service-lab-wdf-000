c87108401bafc416624d2363c0f4cf450efe1adb
antoinfive <antoinfive@gmail.com>
2016-08-30 16:06:45 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,9 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'Antoin Campbell';
+
+	$timeout(function(){
+		$scope.name = "Scott Mescudi"	
+	}, 5000);
 }
 
 angular
